6f547c5ed26b079e79d296b225c9f62a95ad07cf
Daniel Niczyporuk <niczyporuk996@gmail.com>
2021-08-17 10:35:53 +0200

    init repo

---
+++ src/main/java/com/github/danieln96/github/user/api/GithubUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.danieln96.github.user.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/danieln96/github/user/api/GithubUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danieln96.github.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
